cfa10a62d2a749beee1f6370e5cd9ef274405773
DataHearth <antoine.l@antoine-langlois.net>
2021-04-30 20:32:34 +0200

    remove fake main entrypoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	portainertemplates "github.com/datahearth/portainer-templates"
+	"github.com/datahearth/portainer-templates/pkg/db"
+	"github.com/datahearth/portainer-templates/pkg/server"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -9,7 +11,21 @@ var rootCmd = cobra.Command{
 	Use:   "portainer-templates",
 	Short: "portainer-templates is a program that allows user to manage Portainer templates",
 	Run: func(cmd *cobra.Command, args []string) {
-		portainertemplates.Start()
+		logger := logrus.StandardLogger()
+		db, err := db.NewDB(logger)
+		if err != nil {
+			logger.WithError(err).Errorln("failed to create a database instance")
+		}
+
+		srv, err := server.NewServer(logger, db)
+		if err != nil {
+			logger.WithError(err).Errorln("failed to create a server instance")
+		}
+
+		srv.RegisterHandlers()
+		if err := srv.Start(":4345"); err != nil {
+			logger.WithError(err).Errorln("failed to create a server instance")
+		}
 	},
 }
 
